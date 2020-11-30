4193d18251d6b426f5e2bb70188b55a35d72f210
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-11-30 10:42:23 +0100

    Add files via upload

---
+++ src/controller/mod.rs
@@ -0,0 +1,36 @@
+use actix_web::{HttpRequest, HttpResponse, Responder, get,web};
+use isahc::prelude::*;
+const TOKEN:&str="1uLQHB7NKvy508EBLSfDXs-f54JX8FR-JcxE904OE54";
+
+#[get("/plants")]
+pub async fn plants(req:HttpRequest)-> impl Responder{
+    
+    match isahc::get(format!("https://trefle.io/api/v1/plants?token={}",TOKEN)){
+        Ok(mut response)=>{
+            response.text().ok().unwrap()
+        }
+        Err(e)=>{
+            e.to_string()
+        }
+
+    }
+    
+
+}
+
+#[get("/search/{query}")]
+pub async fn search(req:HttpRequest,query:web::Path<String>)-> impl Responder{
+    
+    match isahc::get(format!("https://trefle.io/api/v1/plants?token={}&q={}",TOKEN,query)){
+        
+        Ok(mut response)=>{
+            response.text().ok().unwrap()
+        }
+        Err(e)=>{
+            e.to_string()
+        }
+
+    }
+    
+
+}
