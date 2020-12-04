62a9064888041606956fc8a5dcf51205a22e7714
MedAmineFouzai <54685665+MedAmineFouzai@users.noreply.github.com>
2020-12-04 21:00:09 +0100

    Update mod.rs

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,27 +1,28 @@
-use mongodb::{Collection, Cursor, bson::{Document, doc, oid::ObjectId}, error::Error, results::InsertOneResult};
+use mongodb::{Collection, error::Error, results::InsertOneResult};
+use bson::{Document, doc, oid::ObjectId};
 
 #[derive(Debug,Clone)]
-pub struct User{
+pub struct UserModel{
+
     collection:Collection,
 }
 
+impl UserModel {
 
-impl User {
-
-    pub fn new(collection:Collection)->User {
-        User{collection}
+    pub fn new(collection:Collection)->UserModel {
+        UserModel{collection}
     }
 
-    pub async  fn login(&self,email:&str,password:&str)->Result<Cursor,Error>{
+    pub async  fn login(&self,email:&str,password:&str)->Result<Option<Document>,Error>{
 
-     
+        // self.collection.find(doc!{},None).await?
         
         Ok(
-            self.collection.find(
+            self.collection.find_one(
                 doc!{
                             "email":email,
                             "password":password
-                    },None).await?
+                    },None).unwrap()
         )
 
     }
@@ -32,8 +33,9 @@ impl User {
                 doc!{
                             "username":username,
                             "email":email,
-                            "password":password
-                    },None).await?
+                            "password":password,
+                            "favorites":[]
+                    },None).unwrap()
         )
 
     }
@@ -45,13 +47,13 @@ impl User {
                 doc!{
                     "_id":ObjectId::with_string(user_id).unwrap()
                         },None
-                    ).await?
+                    ).unwrap()
     
         )
 
     }
 
-    pub async fn update_account(&self,user_id:&str,username:&str)->Result<Option<Document>,Error>{
+    pub async fn update_account(&self,user_id:&str,username:&str,email:&str)->Result<Option<Document>,Error>{
         
         Ok(
         
@@ -59,12 +61,31 @@ impl User {
             "_id":ObjectId::with_string(user_id).unwrap()
         },doc!{
           "$set":{
-              "username":username
+              "username":username,
+              "email":email
           }  
-        },None).await?
+        },None).unwrap()
 
     )
     }
+
+    pub async fn update_password(&self,user_id:&str,password:&str)->Result<Option<Document>,Error>{
+        
+        Ok(
+        
+        self.collection.find_one_and_update(doc!{
+            "_id":ObjectId::with_string(user_id).unwrap()
+        },doc!{
+          "$set":{
+              "password":password,
+       
+          }  
+        },None).unwrap()
+
+    )
+    }
+
+
 
     pub async fn add_favorite(&self,user_id:&str,post_id:&str)->Result<Option<Document>,Error>{
         Ok(
@@ -74,7 +95,7 @@ impl User {
           "$push":{
               "favorites":post_id
           }  
-        },None).await?
+        },None).unwrap()
     )
     }
    
@@ -87,13 +108,23 @@ impl User {
         self.collection.find_one_and_update(doc!{
             "_id":ObjectId::with_string(user_id).unwrap()
         },doc!{
-            "$pop":{
+            "$pull":{
                 "favorites":post_id
             }
-        },None).await?
+        },None).unwrap()
     )
     }
+    pub async fn verfiy_user(&self,username:&str,email:&str)->Result<Option<Document>,Error>{
 
+
+        Ok(
+
+        self.collection.find_one(doc!{
+            "username":username,
+            "email":email
+        },None).unwrap()
+    )
+    }
 
 }
 
